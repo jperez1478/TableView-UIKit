dc82ede9192ae1d839e89eb81d982d6aba280ff1
jperez1478 <jessicperez3@gmail.com>
2022-01-26 18:51:17 -0600

    -tablevieww set up

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+   
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
 }
 
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = "Hello World \(indexPath.row+1)"
+    cell.imageView?.image = UIImage(systemName: "house")
+    cell.imageView?.tintColor = .systemGreen
+    return cell
+}
+
+}
